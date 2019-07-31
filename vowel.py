fe0dc36bc046c3b1985b70e29b5657d389af00aa
Chakravhiro <52709243+Chakravhiro@users.noreply.github.com>
2019-07-31 19:37:12 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,9 @@
 ch=input()
-if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+if(ch.isalpha==true):
+       if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
        or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
-    print ("Vowel")
+              print ("Vowel")
+       else:
+              print("Consonant")
 else:
-    print("Consonant")
+       print("invalid")
